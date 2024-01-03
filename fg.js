ed9027259f3f1eb4f35fb8e08ae57154ce27cf2d
Wermontt <153793413+Wermontt@users.noreply.github.com>
2024-01-04 00:50:01 +0100

    Update fg.js

--- fg.js
+++ fg.js
@@ -1,7 +1,7 @@
 let text = "";
 const today = new Date();
 const someday = new Date();
-someday.setFullYear(2100, 0, 14);
+someday.setFullYear(2100, 0, 14 , 87);
 
 if (someday > today) {
   text = "Today is before January 14, 2100.";
