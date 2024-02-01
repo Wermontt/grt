31da73c6543495ead81bca3d12c51ce4064fdfe9
Wermontt <153793413+Wermontt@users.noreply.github.com>
2024-02-01 12:29:49 +0100

    Update fg.js

--- fg.js
+++ fg.js
@@ -4,7 +4,7 @@ const someday = new Date();
 someday.setFullYear(1 , 10);
 
 if (someday > today) {
-  text = "Today is best before Janury 4, 21.";
+  text = "Today is best before Jaury 4, 21.";
 } else {
   text = "Today is after January 14, 2100.";
 }
