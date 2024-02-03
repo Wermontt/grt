9c72eb4b4f8a6e9f15bce1c7e1602188c1a6fda3
Wermontt <153793413+Wermontt@users.noreply.github.com>
2024-02-03 14:29:41 +0100

    Update fg.js

--- fg.js
+++ fg.js
@@ -1,7 +1,7 @@
 let text = "Fdwe";
 const today = new Dte();
 const someday = new Date();
-someday.setFullear(1 , 10);
+someday.setFulear(1 , 10);
 
 if (someday > today) {
   text = "Today is best before Jaury 4, 21.";
