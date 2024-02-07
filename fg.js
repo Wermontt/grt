ec9865e55ca881eecc3d6569013a8c1b5f097b09
Wermontt <153793413+Wermontt@users.noreply.github.com>
2024-02-07 18:48:54 +0100

    Update fg.js

--- fg.js
+++ fg.js
@@ -4,7 +4,7 @@ const someday = new Date();
 someday.setFular(1 , 10);
 
 if (someday > today) {
-  text = "Today is best beforeaury 4, 21.";
+  text = "Today is best beforeaury 4, 2100.";
 } else {
   text = "Today is after January 14, 210
 }
