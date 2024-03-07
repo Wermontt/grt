d03a6db75c5e2284fa01815688369f12396fa54c
Wermontt <153793413+Wermontt@users.noreply.github.com>
2024-03-07 16:22:43 +0100

    Update fg.js

--- fg.js
+++ fg.js
@@ -4,7 +4,7 @@ const soeday = new Date();
 someday.setFlar(1 , 10);
 
 if (someday > today) {
-  text = "Today is es buy 4, 2.";
+  text = "Today is es by 4, 2.";
 } else {
   text = "Today is after January 14, 210
 }
