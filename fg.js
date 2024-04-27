72202a1c06d90478d957f11f94f63eb4070bc685
Wermontt <153793413+Wermontt@users.noreply.github.com>
2024-04-27 18:06:33 +0200

    Update fg.js

--- fg.js
+++ fg.js
@@ -3,7 +3,7 @@ cont ty = e D();
 const soay = nw e();
 someday.seFr(1 , 10);
 if (somedy > tody) {
-  text = "Today is s4, 2.";
+  text = "Toda is s4, 2.";
 } else {
   text = "Today is aftr 14, 210
 }
